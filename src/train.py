0ddf8a306b16d6e3404549f282e3c833a872f966
Fede <palettafederico34@gmail.com>
2026-05-13 18:22:36 +0200

    Inserted new utils function for saving and loading weights

--- src/train.py
+++ src/train.py
@@ -1,4 +1,6 @@
 import os 
+os.environ["PYTORCH_ALLOC_CONF"] = "expandable_segments:True"
+
 import numpy as np
 import torch 
 import torch.nn as nn
@@ -11,18 +13,20 @@ from tqdm import tqdm
 from monai.losses import DiceCELoss
 from monai.metrics import DiceMetric
 from monai.inferers import sliding_window_inference
+from torch.optim.lr_scheduler import CosineAnnealingLR
 
 
 from ..models.nets import PriorUNet
 from ..dataset.data_loader import get_loaders
-from ..utilities.utils import get_film_stats, get_grad_norm
+from ..utilities.utils import get_film_stats, get_grad_norm, save_checkpoint, get_window_predictor
+
 
 
 
 # CONFIGURATION 
 
 json_path = "kaggle/working/dataset_kaggle.json"
-checkpoint_dir = "../checkpoints/"
+checkpoint_dir = "../checkpoints"
 
 os.makedirs(checkpoint_dir, exist_ok=True)
 
@@ -60,7 +64,12 @@ def train():
     # Loss function and optimizer
     loss_function = DiceCELoss(to_onehot_y=False, sigmoid=True)
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-5)
+    scheduler = CosineAnnealingLR(optimizer, T_max=max_epochs, eta_min=1e-6)
+
     dice_metric = DiceMetric(include_background=False, reduction="mean")
+
+    loss_history = []
+    dice_history = []
 
     best_dice = -1
     print("Starting training loop...")
@@ -73,7 +82,7 @@ def train():
         model.train()
         epoch_loss = 0
 
-        progress_bar = tqdm(train_loader, desc=f"training", leave=False)
+        progress_bar = tqdm(train_loader, desc=f"training", leave=False, dynamic_ncols=True)
 
         for step, batch in enumerate(progress_bar, 1):
             inputs = batch["image"].to(device)
@@ -86,22 +95,32 @@ def train():
             loss = loss_function(outputs, labels)
             loss.backward()
 
+            raw_model = model.module if hasattr(model, "module") else model
+
             # Monitora spinta gradienti su Film
             g_norm = get_grad_norm(model)
 
             optimizer.step()
 
             # Monitora quanto sono "cresciuti" i pesi del FiLM
-            w_norm = get_film_stats(model)
-
-            print(f"Metrics - FiLM Grad Norm: {g_norm:.6f}, FiLM Weight Norm: {w_norm:.6f}")
+            w_norm = get_film_stats(raw_model)
 
             epoch_loss += loss.item()
 
-            progress_bar.set_postfix({"Loss_Batch": f"{loss.item():.4f}"})
+            progress_bar.set_postfix({
+                "Loss": f"{loss.item():.4f}",
+                "G": f"{g_norm:.5f}",
+                "W": f"{w_norm:.5f}"
+               })
 
         epoch_loss /= step
+        loss_history.append(epoch_loss)
         print(f"[*] Fine Epoca {epoch} - Training Loss Media: {epoch_loss:.4f}")
+
+        del inputs, labels, priors, outputs, loss
+        torch.cuda.empty_cache()
+
+        scheduler.step()
 
         
         if epoch % val_interval == 0:
@@ -115,19 +134,25 @@ def train():
                 for val_batch in val_progress_bar:
                     val_images = val_batch["image"].to(device)
                     val_labels = val_batch["label"].to(device)
-                    val_priors = val_batch["prior"].view(-1, 1).float().to(device)
+                    val_priors = val_batch["prior"].float().to(device)
+
+                    # Usiamo questa funzione 
+                    predictor = get_window_predictor(model, val_priors)
+
 
                     val_outputs = sliding_window_inference(
                         val_images, 
                         (128,128,128), 
                         sw_batch_size=1, 
-                        predictor = lambda x: model(x, val_priors) # Passiamo i prior qui!
+                        predictor = predictor 
                         )
                     
                     val_outputs = (torch.sigmoid(val_outputs) > 0.5).float()
                     dice_metric(y_pred=val_outputs, y=val_labels)
 
                 metric = dice_metric.aggregate().item()
+                dice_history.append(metric)
+
                 dice_metric.reset()
                 
                 print(f">>> VALIDAZIONE Epoca {epoch} completata! Dice Score: {metric:.4f}")
@@ -135,8 +160,13 @@ def train():
                 # Salvataggio del modello migliore
                 if metric > best_dice:
                     best_dice = metric
-                    torch.save(model.state_dict(), os.path.join(checkpoint_dir, "best_model.pth"))
+                    save_checkpoint(epoch, model, optimizer, scheduler, best_dice, loss_history, dice_history, checkpoint_dir)
                     print(f"    [!] Nuovo record di Dice Score! Modello salvato in {checkpoint_dir}")
+                
+                del val_images, val_labels, val_priors, val_outputs
+                # Svuotiamo la cache della GPU fisicamente
+                torch.cuda.empty_cache()
+                
 
 if __name__ == "__main__":
     train()
--- utilities/utils.py
+++ utilities/utils.py
@@ -1,6 +1,10 @@
 import torch 
+import os 
+
 import torch.nn as nn
 import numpy as np
+
+from collections import OrderedDict
 
 
 
@@ -31,3 +35,86 @@ def get_grad_norm(model):
             total_grad_norm += p.grad.data.norm(2).item()
             count += 1
     return total_grad_norm / count if count > 0 else 0.0
+
+def save_checkpoint(
+        epoch, 
+        model, 
+        optimizer, 
+        scheduler, 
+        dice_best, 
+        loss_history,
+        dice_history,
+        checkpoint_dir
+     ):
+    
+    if hasattr(model, 'module'):
+        state_dict = model.module.state_dict()
+    else:
+        state_dict = model.state_dict()
+    
+    state = {
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state,
+        'scheduler_state_dict': scheduler.state_dict(),
+        'dice_best': dice_best,
+        'loss_history': loss_history,
+        'dice_history': dice_history
+    
+    }
+
+    # checkpoint_dir configuration 
+    os.makedir(os.path.dirname(checkpoint_dir), exist_ok=True)
+
+    torch.save(state, checkpoint_dir)
+
+def load_checkpoint(filepath, model, optimizer=None, scheduler=None):
+    """
+    Carica i pesi adattandosi a qualsiasi configurazione (1 GPU o 2+ GPU).
+    """
+    if not os.path.exists(filepath):
+        print(f"Error: {filepath} not found.")
+        return None
+
+    checkpoint = torch.load(filepath, map_location='cpu') # Carica su CPU per sicurezza
+    state_dict = checkpoint['model_state_dict']
+
+    # 1. Se il file ha 'module.' ma il modello attuale NO
+    new_state_dict = OrderedDict()
+    curr_has_module = hasattr(model, 'module')
+    
+    for k, v in state_dict.items():
+        name = k
+        if k.startswith('module.') and not curr_has_module:
+            name = k[7:] # rimuove 'module.'
+        elif not k.startswith('module.') and curr_has_module:
+            name = 'module.' + k # aggiunge 'module.'
+        new_state_dict[name] = v
+
+    # Carica i pesi modificati
+    model.load_state_dict(new_state_dict)
+    
+    if optimizer and 'optimizer_state_dict' in checkpoint:
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    if scheduler and 'scheduler_state_dict' in checkpoint:
+        scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+        
+    print(f"Checkpoint caricato correttamente (Epoca {checkpoint['epoch']})")
+    return checkpoint
+
+def get_window_predictor(model, priors):
+    """
+    Factory function che restituisce il predictor per la sliding window.
+    Gestisce automaticamente l'espansione dei prior in base al batch size della patch.
+    """
+    def window_pred(patch_batch):
+        # Estrae quanti campioni sta analizzando la sliding window in questo momento
+        current_batch_size = patch_batch.shape[0]
+        
+        # Clona i prior per farli combaciare con il batch size delle patch
+        expanded_priors = priors.repeat(current_batch_size, 1)
+        
+        # Esegue il forward pass sul modello
+        return model(patch_batch, expanded_priors)
+        
+    return window_pred
